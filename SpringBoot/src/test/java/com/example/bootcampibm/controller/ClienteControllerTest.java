db0e041861a368c0142ce8308364efc13b891d7c
Major2571 <caroline.tacats62@gmail.com>
2023-08-04 14:12:44 -0300

    Added documentation for the Cliente controller test

--- SpringBoot/src/test/java/com/example/bootcampibm/controller/ClienteControllerTest.java
+++ SpringBoot/src/test/java/com/example/bootcampibm/controller/ClienteControllerTest.java
@@ -17,7 +17,16 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ClienteControllerTest.class)
+/**
+ * Este arquivo é uma classe de testes para o controlador 'ClienteController' no pacote 'com.example.bootcampibm.controller'.
+ *
+ * A anotação '@WebMvcTest(ClienteController.class)' indica que este teste é um teste de integração para o controlador 'ClienteController'.
+ * Ele permite testar o comportamento do controlador sem carregar toda a aplicação Spring, focando apenas no comportamento da camada de controle.
+ *
+ * Neste teste, são utilizados os recursos do Mockito e do Spring Test para simular as respostas do serviço e realizar as chamadas aos endpoints do controlador.
+ */
+
+@WebMvcTest(ClienteController.class)
 public class ClienteControllerTest {
 
     @Autowired
@@ -29,6 +38,11 @@ public class ClienteControllerTest {
     @InjectMocks
     private ClienteControllerTest clienteController;
 
+    /**
+     * Teste para a operação de inserção de cliente.
+     * O método simula uma requisição HTTP POST para o endpoint '/clientes' com um objeto JSON representando o cliente a ser inserido.
+     * O método espera que o status retornado seja '201 Created' e que o cabeçalho da resposta contenha o local onde o novo cliente foi criado.
+     */
     @Test
     public void testInsertCliente() throws Exception {
         Cliente clienteInserido = new Cliente(1, "maria", "maria@email.com");
@@ -42,11 +56,21 @@ public class ClienteControllerTest {
             .andExpect(MockMvcResultMatchers.header().string("Location", "http://localhost/clientes/1"));
     }
 
+    /**
+     * Teste para a operação de exclusão de cliente.
+     * O método simula uma requisição HTTP DELETE para o endpoint '/clientes/1', onde '1' é o ID do cliente a ser excluído.
+     * O método espera que o status retornado seja '204 No Content', indicando que a operação foi bem-sucedida e não há conteúdo para ser retornado.
+     */
     @Test
     public void testDeleteCliente() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/clientes/1")).andExpect(status().isNoContent());
     }
 
+    /**
+     * Teste para a operação de busca de cliente por ID.
+     * O método simula uma requisição HTTP GET para o endpoint '/clientes/1', onde '1' é o ID do cliente a ser buscado.
+     * O método espera que o status retornado seja '200 OK' e que o corpo da resposta contenha os dados do cliente encontrado, como o ID, nome e email.
+     */
     @Test
     public void testFindClienteByIdExistente() throws Exception {
         Cliente clienteEncontrado = new Cliente(1, "maria", "maria@email.com");
