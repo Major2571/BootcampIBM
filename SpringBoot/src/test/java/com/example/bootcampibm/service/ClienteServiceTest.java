6f75885d1c3d16ff8bb70a1f999cbe2289276f77
Major2571 <caroline.tacats62@gmail.com>
2023-08-04 00:25:03 -0300

    Add documentation to ClienteServiceTest

--- SpringBoot/src/test/java/com/example/bootcampibm/service/ClienteServiceTest.java
+++ SpringBoot/src/test/java/com/example/bootcampibm/service/ClienteServiceTest.java
@@ -21,6 +21,10 @@ import com.example.bootcampibm.domain.Cliente;
 import com.example.bootcampibm.repository.ClienteRepository;
 import com.example.bootcampibm.service.exceptions.ObjectNotFoundException;
 
+/**
+ * Classe de teste para o serviço ClienteService.
+ * Utiliza Mockito para simular o comportamento do repositório.
+ */
 @SpringBootTest
 public class ClienteServiceTest {
 
@@ -30,10 +34,15 @@ public class ClienteServiceTest {
     @InjectMocks
     private ClienteService clienteService;
 
+    /**
+     * Testa o método insert do ClienteService.
+     * Verifica se o cliente foi inserido corretamente e se os atributos estão
+     * corretos.
+     */
     @Test
-    public void testInsertCliente(){
+    public void testInsertCliente() {
 
-        Cliente cliente = new Cliente( 1, "teste", "teste@email");   
+        Cliente cliente = new Cliente(1, "teste", "teste@email");
 
         Mockito.when(clienteRepository.save(any(Cliente.class))).thenReturn(cliente);
 
@@ -47,8 +56,13 @@ public class ClienteServiceTest {
         verify(clienteRepository, times(1)).save(any(Cliente.class));
     }
 
+    /**
+     * Testa o método find do ClienteService quando o cliente com o ID existe.
+     * Verifica se o cliente foi encontrado corretamente e se os atributos estão
+     * corretos.
+     */
     @Test
-    public void testFindClienteByIdExistente(){
+    public void testFindClienteByIdExistente() {
         Integer clienteId = 1;
         Cliente cliente = new Cliente(clienteId, "Jose", "jose@email.com");
 
@@ -64,8 +78,12 @@ public class ClienteServiceTest {
         verify(clienteRepository, times(1)).findById(clienteId);
     }
 
+    /**
+     * Testa o método find do ClienteService quando o cliente com o ID não existe.
+     * Verifica se é lançada uma exceção ObjectNotFoundException.
+     */
     @Test
-    public void testFindClienteByIdNaoExistente(){
+    public void testFindClienteByIdNaoExistente() {
         Integer clienteId = 1;
 
         when(clienteRepository.findById(clienteId)).thenReturn(Optional.empty());
@@ -73,15 +91,24 @@ public class ClienteServiceTest {
         verify(clienteRepository, times(1)).findById(clienteId);
     }
 
+    /**
+     * Testa o método delete do ClienteService.
+     * Verifica se o método clienteRepository.deleteById foi chamado corretamente.
+     */
     @Test
-    public void testDeleteCliente(){
+    public void testDeleteCliente() {
         Integer clienteId = 1;
         clienteService.delete(clienteId);
         verify(clienteRepository, times(1)).deleteById(clienteId);
     }
 
+    /**
+     * Testa o método update do ClienteService.
+     * Verifica se o cliente foi atualizado corretamente e se os atributos estão
+     * corretos.
+     */
     @Test
-    public void testUdateCliente(){
+    public void testUdateCliente() {
         Integer clienteId = 1;
         Cliente clienteAtualizado = new Cliente(clienteId, "Jose", "jose@email.com");
         Cliente clienteExistente = new Cliente(clienteId, "Rafa", "rafa@email.com");
