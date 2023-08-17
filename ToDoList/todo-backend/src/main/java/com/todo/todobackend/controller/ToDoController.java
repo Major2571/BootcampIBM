8da502d008250dfb3eaba31f8263ff43c0eb90cb
Major2571 <caroline.tacats62@gmail.com>
2023-08-16 23:17:16 -0300

    Add controller get

--- ToDoList/todo-backend/src/main/java/com/todo/todobackend/controller/ToDoController.java
+++ ToDoList/todo-backend/src/main/java/com/todo/todobackend/controller/ToDoController.java
@@ -1,9 +1,13 @@
 package com.todo.todobackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.todo.todobackend.domain.ToDo;
 import com.todo.todobackend.service.ToDoService;
 
 @RestController
@@ -13,5 +17,10 @@ public class ToDoController {
     @Autowired
     private ToDoService toDoService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ToDo> findById(@PathVariable Integer id){
+        ToDo obj = toDoService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 
 }
