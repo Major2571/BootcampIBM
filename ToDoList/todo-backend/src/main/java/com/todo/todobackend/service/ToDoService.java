01860bc0bde133255725e8a95166a95428e2d4e2
Major2571 <caroline.tacats62@gmail.com>
2023-08-16 23:17:16 -0300

    Add service findById

--- ToDoList/todo-backend/src/main/java/com/todo/todobackend/service/ToDoService.java
+++ ToDoList/todo-backend/src/main/java/com/todo/todobackend/service/ToDoService.java
@@ -1,6 +1,21 @@
 package com.todo.todobackend.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.todo.todobackend.domain.ToDo;
+import com.todo.todobackend.repository.ToDoRepository;
+import com.todo.todobackend.service.exceptions.ObjectNotFoundException;
+
 public class ToDoService {
 
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    public ToDo findById(Integer id) {
+        Optional<ToDo> obj = toDoRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado: " + id + ", Tipo: "+  ToDo.class.getName()));
+    }
     
 }
