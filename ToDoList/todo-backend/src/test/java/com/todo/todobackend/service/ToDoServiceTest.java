bfa0abdafc0d988e315bec6f35217855539453c2
Major2571 <caroline.tacats62@gmail.com>
2023-08-23 23:17:16 -0300

    Add service test find by id

--- ToDoList/todo-backend/src/test/java/com/todo/todobackend/service/ToDoServiceTest.java
+++ ToDoList/todo-backend/src/test/java/com/todo/todobackend/service/ToDoServiceTest.java
@@ -1,3 +1,28 @@
 package com.todo.todobackend.service;
+@SpringBootTest
 public class ToDoServiceTest {
+    @Mock
+    private ToDoRepository toDoRepository;
+
+    @InjectMocks
+    private ToDoService toDoService;
+
+    @Test
+    public void testFindById(){
+
+        Integer id = 1;
+
+        ToDo mockToDo = new ToDo();
+        mockToDo.setId(id);
+
+        when(toDoRepository.findById(id)).thenReturn(Optional.of(mockToDo));
+
+        ToDo result = toDoService.findById(id);
+
+        assertEquals(mockToDo, result);
+
+        verify(toDoRepository, times(1)).findById(id);
+
+    }
+
 }
