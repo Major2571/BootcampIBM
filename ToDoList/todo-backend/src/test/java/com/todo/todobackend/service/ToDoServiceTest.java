f793d4633423498e896e26c53be17faa6838f3c5
Major2571 <caroline.tacats62@gmail.com>
2023-08-24 23:17:16 -0300

    Add all missing imports

--- ToDoList/todo-backend/src/test/java/com/todo/todobackend/service/ToDoServiceTest.java
+++ ToDoList/todo-backend/src/test/java/com/todo/todobackend/service/ToDoServiceTest.java
@@ -1,6 +1,30 @@
 package com.todo.todobackend.service;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.todo.todobackend.domain.ToDo;
+import com.todo.todobackend.repository.ToDoRepository;
+import com.todo.todobackend.service.exceptions.ObjectNotFoundException;
+
 @SpringBootTest
 public class ToDoServiceTest {
+    
     @Mock
     private ToDoRepository toDoRepository;
 
