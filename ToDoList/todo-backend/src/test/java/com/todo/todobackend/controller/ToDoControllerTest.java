96a6ca48614f806af9f5c1308dd785362900d025
Major2571 <caroline.tacats62@gmail.com>
2023-08-25 23:17:16 -0300

    Start controller test

--- ToDoList/todo-backend/src/test/java/com/todo/todobackend/controller/ToDoControllerTest.java
+++ ToDoList/todo-backend/src/test/java/com/todo/todobackend/controller/ToDoControllerTest.java
@@ -1,3 +1,17 @@
 package com.todo.todobackend.controller;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+@WebMvcTest(ToDoController.class)
 public class ToDoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ToDoService toDoService;
+
+    @InjectMocks
+    private ToDoControllerTest toDoController;
+
 }
