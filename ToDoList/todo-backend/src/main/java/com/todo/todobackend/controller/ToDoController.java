7ef099a8ab6da198d2f569a1bba1b7a34dc49a69
Major2571 <caroline.tacats62@gmail.com>
2023-08-28 23:17:16 -0300

    Add documentation to ToDoController

--- ToDoList/todo-backend/src/main/java/com/todo/todobackend/controller/ToDoController.java
+++ ToDoList/todo-backend/src/main/java/com/todo/todobackend/controller/ToDoController.java
@@ -21,49 +21,89 @@ import com.todo.todobackend.service.ToDoService;
 @RestController
 @RequestMapping("/to-do")
 public class ToDoController {
-    
+
     @Autowired
     private ToDoService toDoService;
 
+    /**
+     * Retrieve a specific ToDo item by its ID.
+     *
+     * @param id The ID of the ToDo item to retrieve.
+     * @return ResponseEntity containing the retrieved ToDo item.
+     */
     @GetMapping("/{id}")
-    public ResponseEntity<ToDo> findById(@PathVariable Integer id){
+    public ResponseEntity<ToDo> findById(@PathVariable Integer id) {
         ToDo obj = toDoService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    /**
+     * Retrieve all open ToDo items.
+     *
+     * @return ResponseEntity containing a list of open ToDo items.
+     */
     @GetMapping("/open")
-    public ResponseEntity<List<ToDo>> findOpen(){
+    public ResponseEntity<List<ToDo>> findOpen() {
         List<ToDo> list = toDoService.findAllOpen();
         return ResponseEntity.ok().body(list);
     }
 
+    /**
+     * Retrieve all closed ToDo items.
+     *
+     * @return ResponseEntity containing a list of closed ToDo items.
+     */
     @GetMapping("/closed")
-    public ResponseEntity<List<ToDo>> findClosed(){
+    public ResponseEntity<List<ToDo>> findClosed() {
         List<ToDo> list = toDoService.findAllClosed();
         return ResponseEntity.ok().body(list);
     }
 
+    /**
+     * Retrieve all ToDo items.
+     *
+     * @return ResponseEntity containing a list of all ToDo items.
+     */
     @GetMapping("/all")
-    public ResponseEntity<List<ToDo>> findAll(){
+    public ResponseEntity<List<ToDo>> findAll() {
         List<ToDo> list = toDoService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
+    /**
+     * Create a new ToDo item.
+     *
+     * @param obj The ToDo item to create.
+     * @return ResponseEntity with the newly created ToDo item's URI.
+     */
     @PostMapping
-    public ResponseEntity<ToDo> create(@RequestBody ToDo obj){
+    public ResponseEntity<ToDo> create(@RequestBody ToDo obj) {
         obj = toDoService.create(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
+    /**
+     * Delete a ToDo item by its ID.
+     *
+     * @param id The ID of the ToDo item to delete.
+     * @return ResponseEntity with a no content status.
+     */
     @DeleteMapping("/{id}/delete")
-    public ResponseEntity<Void> delete(@PathVariable Integer id){
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         toDoService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * Update a ToDo item by its ID.
+     *
+     * @param id  The ID of the ToDo item to update.
+     * @param obj The updated ToDo item.
+     * @return ResponseEntity containing the updated ToDo item.
+     */
     @PutMapping("/{id}/update")
-    public ResponseEntity<ToDo> update(@PathVariable Integer id, @RequestBody ToDo obj){
+    public ResponseEntity<ToDo> update(@PathVariable Integer id, @RequestBody ToDo obj) {
         ToDo attObj = toDoService.update(id, obj);
         return ResponseEntity.ok().body(attObj);
     }
