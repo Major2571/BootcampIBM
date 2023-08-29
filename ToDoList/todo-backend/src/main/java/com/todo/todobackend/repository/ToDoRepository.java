778b2fec8cee71390602ed179a3d7a1cd4edd800
Major2571 <caroline.tacats62@gmail.com>
2023-08-28 23:17:16 -0300

    Add documentation to ToDoRepository

--- ToDoList/todo-backend/src/main/java/com/todo/todobackend/repository/ToDoRepository.java
+++ ToDoList/todo-backend/src/main/java/com/todo/todobackend/repository/ToDoRepository.java
@@ -11,9 +11,19 @@ import com.todo.todobackend.domain.ToDo;
 @Repository
 public interface ToDoRepository  extends JpaRepository<ToDo, Integer> {
     
+    /**
+     * Retrieve a list of open ToDo items.
+     *
+     * @return A list of open ToDo items.
+     */
     @Query("SELECT obj FROM ToDo obj WHERE obj.completed = false ORDER BY obj.dateEnd")
     List<ToDo> findAllOpen();
 
+    /**
+     * Retrieve a list of closed ToDo items.
+     *
+     * @return A list of closed ToDo items.
+     */
     @Query("SELECT obj FROM ToDo obj WHERE obj.completed = true ORDER BY obj.dateEnd")
     List<ToDo> findAllClosed();
 
