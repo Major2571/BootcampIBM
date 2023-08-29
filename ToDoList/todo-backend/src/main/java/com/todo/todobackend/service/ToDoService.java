8bdb3f3922958c5984e10c8de4ebbca8207a82a9
Major2571 <caroline.tacats62@gmail.com>
2023-08-28 23:17:16 -0300

    Add documentation to ToDoService

--- ToDoList/todo-backend/src/main/java/com/todo/todobackend/service/ToDoService.java
+++ ToDoList/todo-backend/src/main/java/com/todo/todobackend/service/ToDoService.java
@@ -16,43 +16,82 @@ public class ToDoService {
     @Autowired
     private ToDoRepository toDoRepository;
 
+    /**
+     * Find a ToDo item by its ID.
+     *
+     * @param id The ID of the ToDo item to find.
+     * @return The retrieved ToDo item.
+     * @throws ObjectNotFoundException if the ToDo item with the given ID is not
+     *                                 found.
+     */
     public ToDo findById(Integer id) {
         Optional<ToDo> obj = toDoRepository.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado: " + id + ", Tipo: "+  ToDo.class.getName()));
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado: " + id + ", Tipo: " + ToDo.class.getName()));
     }
-    
+
+    /**
+     * Find all open ToDo items.
+     *
+     * @return A list of open ToDo items.
+     */
     public List<ToDo> findAllOpen() {
         List<ToDo> list = toDoRepository.findAllOpen();
         return list;
     }
 
+    /**
+     * Find all closed ToDo items.
+     *
+     * @return A list of closed ToDo items.
+     */
     public List<ToDo> findAllClosed() {
         List<ToDo> listaFechada = toDoRepository.findAllClosed();
         return listaFechada;
     }
 
+    /**
+     * Find all ToDo items.
+     *
+     * @return A list of all ToDo items.
+     */
     public List<ToDo> findAll() {
         List<ToDo> list = toDoRepository.findAll();
         return list;
     }
 
+    /**
+     * Create a new ToDo item.
+     *
+     * @param obj The ToDo item to create.
+     * @return The newly created ToDo item.
+     */
     public ToDo create(ToDo obj) {
         obj.setId(null);
         return toDoRepository.save(obj);
     }
 
+    /**
+     * Delete a ToDo item by its ID.
+     *
+     * @param id The ID of the ToDo item to delete.
+     */
     public void delete(Integer id) {
         toDoRepository.deleteById(id);
     }
 
+    /**
+     * Update a ToDo item by its ID.
+     *
+     * @param id  The ID of the ToDo item to update.
+     * @param obj The updated ToDo item.
+     * @return The updated ToDo item.
+     */
     public ToDo update(Integer id, ToDo obj) {
-        
         ToDo attObj = findById(id);
         attObj.setTitle(obj.getTitle());
         attObj.setDescription(obj.getDescription());
         attObj.setDateEnd(obj.getDateEnd());
         attObj.setCompleted(obj.getCompleted());
-
         return toDoRepository.save(attObj);
     }
 
