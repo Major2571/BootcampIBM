97812f153b3c51598c314bbc8e8f3dc479412246
Major2571 <caroline.tacats62@gmail.com>
2023-08-29 23:17:16 -0300

    npm update

--- ToDoList/todo-frontend/src/app/app.module.ts
+++ ToDoList/todo-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { TesteDoisComponent } from './components/teste-dois/teste-dois.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent,
+    TesteDoisComponent
   ],
   imports: [
     BrowserModule,
