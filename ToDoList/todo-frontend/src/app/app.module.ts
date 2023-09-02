d9fed1c7dfcab80c29638fb421f935e5ec572266
Major2571 <caroline.tacats62@gmail.com>
2023-09-01 23:14:34 -0300

    Add angular meterial

--- ToDoList/todo-frontend/src/app/app.module.ts
+++ ToDoList/todo-frontend/src/app/app.module.ts
@@ -7,18 +7,26 @@ import { ToDoListComponent } from './components/to-do-list/to-do-list.component'
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ToDoFormComponent } from './components/to-do-form/to-do-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoListComponent,
+    ToDoFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
