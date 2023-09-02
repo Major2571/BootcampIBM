754ec8b92163842e4cbc64793f5770285c3942d4
Major2571 <caroline.tacats62@gmail.com>
2023-09-01 23:13:54 -0300

    Add form component

---
+++ ToDoList/todo-frontend/src/app/components/to-do-form/to-do-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { toDo } from 'src/app/model/to-do';
+import { ToDoService } from 'src/app/service/to-do.service';
+
+@Component({
+  selector: 'app-to-do-form',
+  templateUrl: './to-do-form.component.html',
+  styleUrls: ['./to-do-form.component.css']
+})
+export class ToDoFormComponent implements OnInit {
+
+  toDo = {} as toDo;
+
+  listToDo: toDo[] = [];
+
+  constructor(private service: ToDoService) { }
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  saveNewToDo(): void {
+    this.service.postToDo(this.toDo)
+      .subscribe(response => {
+        this.listToDo.push(response);
+      });
+  }
+
+}
