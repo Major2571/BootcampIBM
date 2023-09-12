d672d0d71a5f45ee1392f50806efe62bf9e4f694
Major2571 <caroline.tacats62@gmail.com>
2023-09-11 23:17:16 -0300

    Add header

--- ToDoList/todo-frontend/src/app/app.module.ts
+++ ToDoList/todo-frontend/src/app/app.module.ts
@@ -13,11 +13,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoFormComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
